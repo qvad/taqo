74f0d26331f727dd4bd762f1ac228fb4a7dc3f32
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-08-10 15:48:44 +0300

    Added order by

---
+++ sql/basic/queries/order_by.sql
@@ -0,0 +1,10 @@
+SELECT t1.k1, t1.k2, t1.v1, t1.v2 FROM t1 ORDER BY t1.k1, t1.v1 LIMIT 100000;
+SELECT t1.k1, t1.k2, t1.v1, t1.v2 FROM t1 ORDER BY t1.k1, t1.v1 LIMIT 1000000;
+SELECT t2.k1, t2.k2, t2.v1, t2.v2 FROM t2 ORDER BY t2.k1, t2.v1 LIMIT 100000;
+SELECT t2.k1, t2.k2, t2.v1, t2.v2 FROM t2 ORDER BY t2.k1, t2.v1 LIMIT 1000000;
+SELECT t3.k1, t3.k2, t3.v1, t3.v2 FROM t3 ORDER BY t3.k1, t3.v1 LIMIT 100000;
+SELECT t3.k1, t3.k2, t3.v1, t3.v2 FROM t3 ORDER BY t3.k1, t3.v1 LIMIT 1000000;
+SELECT ts2.k1, ts2.k2, ts2.v1, ts2.v2 FROM ts2 ORDER BY ts2.k1, ts2.v1 LIMIT 100000;
+SELECT ts2.k1, ts2.k2, ts2.v1, ts2.v2 FROM ts2 ORDER BY ts2.k1, ts2.v1 LIMIT 1000000;
+SELECT ts3.k1, ts3.k2, ts3.v1, ts3.v2 FROM ts3 ORDER BY ts3.k1, ts3.v1 LIMIT 100000;
+SELECT ts3.k1, ts3.k2, ts3.v1, ts3.v2 FROM ts3 ORDER BY ts3.k1, ts3.v1 LIMIT 1000000;
