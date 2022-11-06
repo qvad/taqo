8f5f6790c33bf4cfeaec20642217cd86a3e5f71b
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-11-06 15:44:34 +0400

    Fixed index colocation

--- sql/basic/create.sql
+++ sql/basic/create.sql
@@ -3,21 +3,21 @@ CREATE TABLE t1(k1 int,
                 v1 int,
                 v2 text,
                 PRIMARY KEY(k1 ASC, k2 ASC)) WITH (colocated = true);
-CREATE INDEX ON t1(v1 ASC, k2 ASC) WITH (colocated = true);
+CREATE INDEX ON t1(v1 ASC, k2 ASC);
 
 CREATE TABLE t2(k1 int,
                 k2 text,
                 v1 int,
                 v2 text,
                 PRIMARY KEY(k1 ASC, k2 ASC)) WITH (colocated = true);
-CREATE INDEX ON t2(v1 ASC, k2 ASC) WITH (colocated = true);
+CREATE INDEX ON t2(v1 ASC, k2 ASC);
 
 CREATE TABLE t3(k1 int,
                 k2 text,
                 v1 int,
                 v2 text,
                 PRIMARY KEY(k1 ASC, k2 ASC)) WITH (colocated = true);
-CREATE INDEX ON t3(v1 ASC, k2 ASC) WITH (colocated = true);
+CREATE INDEX ON t3(v1 ASC, k2 ASC);
 
 CREATE TABLE ts2(k1 int,
                  k2 text,
--- sql/subqueries/create.sql
+++ sql/subqueries/create.sql
@@ -3,10 +3,10 @@ CREATE TABLE t500000 WITH (colocated = true) as select a, md5(random()::text) fr
 CREATE TABLE t50000 WITH (colocated = true) as select a, md5(random()::text) from generate_Series(1,50000) a;
 CREATE TABLE t100 WITH (colocated = true) as select a, md5(random()::text) from generate_Series(1,100) a;
 
-CREATE INDEX t1000000_idx ON t1000000(a) WITH (colocated = true);
-CREATE INDEX t500000_idx ON t500000(a) WITH (colocated = true);
-CREATE INDEX t50000_idx ON t50000(a) WITH (colocated = true);
-CREATE INDEX t100_idx ON t100(a) WITH (colocated = true);
+CREATE INDEX t1000000_idx ON t1000000(a);
+CREATE INDEX t500000_idx ON t500000(a);
+CREATE INDEX t50000_idx ON t50000(a);
+CREATE INDEX t100_idx ON t100(a);
 
 ANALYZE t1000000;
 ANALYZE t500000;
