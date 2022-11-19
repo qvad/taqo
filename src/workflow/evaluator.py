d306a46bf1b113c6f07da97244b09fa6988bec64
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-11-20 00:56:31 +0400

    Fixed timeouts

--- src/workflow/evaluator.py
+++ src/workflow/evaluator.py
@@ -94,7 +94,7 @@ class QueryEvaluator:
         self.logger.debug(f"{len(list_of_optimizations)} optimizations generated")
         progress_bar = tqdm(list_of_optimizations)
         num_skipped = 0
-        min_execution_time = original_query.execution_time_ms if original_query.execution_time_ms > 0 else self.config.max_query_timeout
+        min_execution_time = original_query.execution_time_ms if original_query.execution_time_ms > 0 else (self.config.max_query_timeout * 1000)
         original_query.optimizations = []
         execution_plans_checked = set()
 
