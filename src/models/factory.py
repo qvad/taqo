9ae32d4769179000ecbea57d5ee77d5a00a27e7b
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-12-21 15:01:01 +0400

    Removed complex generator, now it's SQL model

--- src/models/factory.py
+++ src/models/factory.py
@@ -1,12 +1,6 @@
 from config import Config
-from models.complex import ComplexModel
 from models.sql import SQLModel, BasicOpsModel
 
 
 def get_test_model():
-    if Config().model == "complex":
-        return ComplexModel()
-    elif Config().model == "basic":
-        return BasicOpsModel()
-    else:
-        return SQLModel()
+    return BasicOpsModel() if Config().model == "basic" else SQLModel()
