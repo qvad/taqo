459235f5e22f349df524ef538b01be9f3706e520
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-04-05 18:48:47 +0400

    Added index only scans

--- src/db/postgres.py
+++ src/db/postgres.py
@@ -178,7 +178,8 @@ class Leading:
         for table in self.alias_to_table:
             tables_and_idxs = list({f"{Scans.INDEX.value}({table.alias})"
                                     for field in table.fields if field.is_index})
-
+            tables_and_idxs.append({f"{Scans.INDEX_ONLY.value}({table.alias})"
+                                    for field in table.fields if field.is_index})
             tables_and_idxs.append(f"{Scans.SEQ.value}({table.alias})")
             self.table_scan_hints.append(tables_and_idxs)
 
