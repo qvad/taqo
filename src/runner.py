c6ef025d3e8e4b6050b795ac7d46056995fa74d7
Hal Takahara <8877300+mtakahar@users.noreply.github.com>
2023-07-21 12:23:32 -0400

    wip: add experimental "cost" report type for cost validation charts

--- src/runner.py
+++ src/runner.py
@@ -6,6 +6,7 @@ from pyhocon import ConfigFactory
 from config import Config, init_logger, ConnectionConfig, DDLStep
 from db.factory import create_database
 from db.postgres import DEFAULT_USERNAME, DEFAULT_PASSWORD, PostgresResultsLoader
+from reports.cost import CostReport
 from reports.regression import RegressionReport
 from reports.score import ScoreReport
 from reports.selectivity import SelectivityReport
@@ -359,5 +360,9 @@ if __name__ == "__main__":
 
             SelectivityReport.generate_report(default_queries, default_analyze_queries, ta_queries,
                                               ta_analyze_queries, stats_queries, stats_analyze_queries)
+        elif args.type == "cost":
+            yb_queries = loader.get_queries_from_previous_result(args.results)
+            CostReport.generate_report(yb_queries)
+
         else:
             raise AttributeError(f"Unknown test type defined {config.test}")
