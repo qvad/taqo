6a79fc6e775a588448af7c8344fe9493923da039
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-08-15 12:09:23 +0400

    Removed stats.sql files

--- sql/basic/stats.sql
+++
@@ -1,5 +0,0 @@
-SELECT (select max(k1) from t1),
-       (select max(k1) from t2),
-       (select max(k1) from t3),
-       (select max(k1) from ts2),
-       (select max(k1) from ts3);
--- sql/subqueries/stats.sql
+++
@@ -1,4 +0,0 @@
-select (select count(*) from t1000000),
-       (select count(*) from t500000),
-       (select count(*) from t50000),
-       (select count(*) from t100);
