d2c3e7b061ba63908eabdaef7694b895d81441b7
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-08-18 20:02:57 +0400

    WIP

--- src/db/yugabyte.py
+++ src/db/yugabyte.py
@@ -49,6 +49,9 @@ class Yugabyte(Postgres):
         self.logger.info(f"Evaluating compaction on tables {[table.name for table in tables]}")
 
         for table in tables:
+            subprocess.call(f'ls -la',
+                            shell=True,
+                            cwd=self.config.yugabyte_bin_path)
             result = subprocess.call(f'./yb-admin -master_addresses {self.config.connection.host}:7100 '
                                      f'compact_table ysql.{self.config.connection.database} {table.name}',
                                      shell=True,
