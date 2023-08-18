16365a2353ed5ccad0dc50c8060196ae44356d2d
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-08-18 16:35:26 +0400

    DEBUG

--- src/db/yugabyte.py
+++ src/db/yugabyte.py
@@ -47,8 +47,8 @@ def yb_db_factory(config):
 class Yugabyte(Postgres):
     def run_compaction(self, tables: list[str]):
         self.logger.info(f"Evaluating compaction on tables {[table.name for table in tables]}")
-        print(subprocess.call("pwd"))
-        print(subprocess.call("ls -la bin/yugabyte/yb-admin"))
+        self.logger.info(subprocess.call("pwd"))
+        self.logger.info(subprocess.call("ls -la"))
         for table in tables:
             result = subprocess.call(['bin/yugabyte/yb-admin',
                                       '-master_addresses', f"{self.config.connection.host}:7100",
