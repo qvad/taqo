7d5ad12e496f207f508acd299b3252429c6a58ea
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-12-05 17:28:56 +0400

    Changing DDL to ASC explicitly

--- sql/join-order-benchmark/postgres.create.sql
+++ sql/join-order-benchmark/postgres.create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE aka_name (
-                          id integer NOT NULL PRIMARY KEY ASC,
+                          id integer NOT NULL PRIMARY KEY,
                           person_id integer NOT NULL,
                           name text NOT NULL,
                           imdb_index character varying(12),
@@ -10,7 +10,7 @@ CREATE TABLE aka_name (
 );
 
 CREATE TABLE aka_title (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            movie_id integer NOT NULL,
                            title text NOT NULL,
                            imdb_index character varying(12),
@@ -25,7 +25,7 @@ CREATE TABLE aka_title (
 );
 
 CREATE TABLE cast_info (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            person_id integer NOT NULL,
                            movie_id integer NOT NULL,
                            person_role_id integer,
@@ -35,7 +35,7 @@ CREATE TABLE cast_info (
 );
 
 CREATE TABLE char_name (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            name text NOT NULL,
                            imdb_index character varying(12),
                            imdb_id integer,
@@ -45,12 +45,12 @@ CREATE TABLE char_name (
 );
 
 CREATE TABLE comp_cast_type (
-                                id integer NOT NULL PRIMARY KEY ASC,
+                                id integer NOT NULL PRIMARY KEY,
                                 kind character varying(32) NOT NULL
 );
 
 CREATE TABLE company_name (
-                              id integer NOT NULL PRIMARY KEY ASC,
+                              id integer NOT NULL PRIMARY KEY,
                               name text NOT NULL,
                               country_code character varying(255),
                               imdb_id integer,
@@ -60,40 +60,40 @@ CREATE TABLE company_name (
 );
 
 CREATE TABLE company_type (
-                              id integer NOT NULL PRIMARY KEY ASC,
+                              id integer NOT NULL PRIMARY KEY,
                               kind character varying(32) NOT NULL
 );
 
 CREATE TABLE complete_cast (
-                               id integer NOT NULL PRIMARY KEY ASC,
+                               id integer NOT NULL PRIMARY KEY,
                                movie_id integer,
                                subject_id integer NOT NULL,
                                status_id integer NOT NULL
 );
 
 CREATE TABLE info_type (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            info character varying(32) NOT NULL
 );
 
 CREATE TABLE keyword (
-                         id integer NOT NULL PRIMARY KEY ASC,
+                         id integer NOT NULL PRIMARY KEY,
                          keyword text NOT NULL,
                          phonetic_code character varying(5)
 );
 
 CREATE TABLE kind_type (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            kind character varying(15) NOT NULL
 );
 
 CREATE TABLE link_type (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            link character varying(32) NOT NULL
 );
 
 CREATE TABLE movie_companies (
-                                 id integer NOT NULL PRIMARY KEY ASC,
+                                 id integer NOT NULL PRIMARY KEY,
                                  movie_id integer NOT NULL,
                                  company_id integer NOT NULL,
                                  company_type_id integer NOT NULL,
@@ -101,7 +101,7 @@ CREATE TABLE movie_companies (
 );
 
 CREATE TABLE movie_info (
-                            id integer NOT NULL PRIMARY KEY ASC,
+                            id integer NOT NULL PRIMARY KEY,
                             movie_id integer NOT NULL,
                             info_type_id integer NOT NULL,
                             info text NOT NULL,
@@ -109,7 +109,7 @@ CREATE TABLE movie_info (
 );
 
 CREATE TABLE movie_info_idx (
-                                id integer NOT NULL PRIMARY KEY ASC,
+                                id integer NOT NULL PRIMARY KEY,
                                 movie_id integer NOT NULL,
                                 info_type_id integer NOT NULL,
                                 info text NOT NULL,
@@ -117,20 +117,20 @@ CREATE TABLE movie_info_idx (
 );
 
 CREATE TABLE movie_keyword (
-                               id integer NOT NULL PRIMARY KEY ASC,
+                               id integer NOT NULL PRIMARY KEY,
                                movie_id integer NOT NULL,
                                keyword_id integer NOT NULL
 );
 
 CREATE TABLE movie_link (
-                            id integer NOT NULL PRIMARY KEY ASC,
+                            id integer NOT NULL PRIMARY KEY,
                             movie_id integer NOT NULL,
                             linked_movie_id integer NOT NULL,
                             link_type_id integer NOT NULL
 );
 
 CREATE TABLE name (
-                      id integer NOT NULL PRIMARY KEY ASC,
+                      id integer NOT NULL PRIMARY KEY,
                       name text NOT NULL,
                       imdb_index character varying(12),
                       imdb_id integer,
@@ -142,7 +142,7 @@ CREATE TABLE name (
 );
 
 CREATE TABLE person_info (
-                             id integer NOT NULL PRIMARY KEY ASC,
+                             id integer NOT NULL PRIMARY KEY,
                              person_id integer NOT NULL,
                              info_type_id integer NOT NULL,
                              info text NOT NULL,
@@ -150,12 +150,12 @@ CREATE TABLE person_info (
 );
 
 CREATE TABLE role_type (
-                           id integer NOT NULL PRIMARY KEY ASC,
+                           id integer NOT NULL PRIMARY KEY,
                            role character varying(32) NOT NULL
 );
 
 CREATE TABLE title (
-                       id integer NOT NULL PRIMARY KEY ASC,
+                       id integer NOT NULL PRIMARY KEY,
                        title text NOT NULL,
                        imdb_index character varying(12),
                        kind_id integer NOT NULL,
@@ -169,26 +169,26 @@ CREATE TABLE title (
                        md5sum character varying(32)
 );
 
-create index company_id_movie_companies on movie_companies(company_id ASC);
-create index company_type_id_movie_companies on movie_companies(company_type_id ASC);
-create index info_type_id_movie_info_idx on movie_info_idx(info_type_id ASC);
-create index info_type_id_movie_info on movie_info(info_type_id ASC);
-create index info_type_id_person_info on person_info(info_type_id ASC);
-create index keyword_id_movie_keyword on movie_keyword(keyword_id ASC);
-create index kind_id_aka_title on aka_title(kind_id ASC);
-create index kind_id_title on title(kind_id ASC);
-create index linked_movie_id_movie_link on movie_link(linked_movie_id ASC);
-create index link_type_id_movie_link on movie_link(link_type_id ASC);
-create index movie_id_aka_title on aka_title(movie_id ASC);
-create index movie_id_cast_info on cast_info(movie_id ASC);
-create index movie_id_complete_cast on complete_cast(movie_id ASC);
-create index movie_id_movie_companies on movie_companies(movie_id ASC);
-create index movie_id_movie_info_idx on movie_info_idx(movie_id ASC);
-create index movie_id_movie_keyword on movie_keyword(movie_id ASC);
-create index movie_id_movie_link on movie_link(movie_id ASC);
-create index movie_id_movie_info on movie_info(movie_id ASC);
-create index person_id_aka_name on aka_name(person_id ASC);
-create index person_id_cast_info on cast_info(person_id ASC);
-create index person_id_person_info on person_info(person_id ASC);
-create index person_role_id_cast_info on cast_info(person_role_id ASC);
-create index role_id_cast_info on cast_info(role_id) ASC;
+create index company_id_movie_companies on movie_companies(company_id);
+create index company_type_id_movie_companies on movie_companies(company_type_id);
+create index info_type_id_movie_info_idx on movie_info_idx(info_type_id);
+create index info_type_id_movie_info on movie_info(info_type_id);
+create index info_type_id_person_info on person_info(info_type_id);
+create index keyword_id_movie_keyword on movie_keyword(keyword_id);
+create index kind_id_aka_title on aka_title(kind_id);
+create index kind_id_title on title(kind_id);
+create index linked_movie_id_movie_link on movie_link(linked_movie_id);
+create index link_type_id_movie_link on movie_link(link_type_id);
+create index movie_id_aka_title on aka_title(movie_id);
+create index movie_id_cast_info on cast_info(movie_id);
+create index movie_id_complete_cast on complete_cast(movie_id);
+create index movie_id_movie_companies on movie_companies(movie_id);
+create index movie_id_movie_info_idx on movie_info_idx(movie_id);
+create index movie_id_movie_keyword on movie_keyword(movie_id);
+create index movie_id_movie_link on movie_link(movie_id);
+create index movie_id_movie_info on movie_info(movie_id);
+create index person_id_aka_name on aka_name(person_id);
+create index person_id_cast_info on cast_info(person_id);
+create index person_id_person_info on person_info(person_id);
+create index person_role_id_cast_info on cast_info(person_role_id);
+create index role_id_cast_info on cast_info(role_id);
