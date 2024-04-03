659eb07d5c8ecd84a5945237ec96b7dc41c497e1
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2024-04-03 21:28:31 +0400

    Fixed stats report to return error

--- src/actions/reports/score_stats.py
+++ src/actions/reports/score_stats.py
@@ -30,7 +30,6 @@ class ScoreStatsReport(AbstractReportAction):
         else:
             self.queries[query.tag].append([query, pg])
 
-
     def build_report(self, version, git_hash):
         yb_bests = 0
         pg_bests = 0
@@ -44,42 +43,45 @@ class ScoreStatsReport(AbstractReportAction):
         inconsistent_results = 0
         total = 0
 
-        for queries in self.queries.values():
-            for query in queries:
-                yb_query = query[0]
-                pg_query = query[1]
+        try:
+            for queries in self.queries.values():
+                for query in queries:
+                    yb_query = query[0]
+                    pg_query = query[1]
 
-                yb_best = yb_query.get_best_optimization(self.config)
-                pg_best = pg_query.get_best_optimization(self.config)
+                    yb_best = yb_query.get_best_optimization(self.config)
+                    pg_best = pg_query.get_best_optimization(self.config)
 
-                inconsistent_results += 1 if yb_query.get_inconsistent_results() else 0
+                    inconsistent_results += 1 if yb_query.get_inconsistent_results() else 0
 
-                pg_success = pg_query.execution_time_ms > 0
+                    pg_success = pg_query.execution_time_ms > 0
 
-                qe_default_geo *= yb_query.execution_time_ms / pg_query.execution_time_ms if pg_success else 1
-                qe_bests_geo *= yb_best.execution_time_ms / pg_best.execution_time_ms if pg_success else 1
+                    qe_default_geo *= yb_query.execution_time_ms / pg_query.execution_time_ms if pg_success else 1
+                    qe_bests_geo *= yb_best.execution_time_ms / pg_best.execution_time_ms if pg_success else 1
 
-                if yb_query.execution_time_ms > 0 and yb_best.execution_time_ms > 0:
-                    qo_yb_bests_geo *= yb_query.execution_time_ms / yb_best.execution_time_ms
-                if pg_query.execution_time_ms > 0 and pg_best.execution_time_ms > 0:
-                    qo_pg_bests_geo *= pg_query.execution_time_ms / pg_best.execution_time_ms
+                    if yb_query.execution_time_ms > 0 and yb_best.execution_time_ms > 0:
+                        qo_yb_bests_geo *= yb_query.execution_time_ms / yb_best.execution_time_ms
+                    if pg_query.execution_time_ms > 0 and pg_best.execution_time_ms > 0:
+                        qo_pg_bests_geo *= pg_query.execution_time_ms / pg_best.execution_time_ms
 
-                yb_bests += 1 if yb_query.compare_plans(yb_best.execution_plan) else 0
-                pg_bests += 1 if pg_success and pg_query.compare_plans(pg_best.execution_plan) else 0
-                timed_out += 1 if yb_query.execution_time_ms == -1 else 0
-                slower_then_10x += 1 if pg_query.execution_time_ms and \
-                                        (yb_query.execution_time_ms / pg_query.execution_time_ms) >= 10 else 0
-                best_slower_then_10x += 1 if pg_query.execution_time_ms and \
-                                             (yb_best.execution_time_ms / pg_query.execution_time_ms) >= 10 else 0
+                    yb_bests += 1 if yb_query.compare_plans(yb_best.execution_plan) else 0
+                    pg_bests += 1 if pg_success and pg_query.compare_plans(pg_best.execution_plan) else 0
+                    timed_out += 1 if yb_query.execution_time_ms == -1 else 0
+                    slower_then_10x += 1 if pg_query.execution_time_ms and \
+                                            (yb_query.execution_time_ms / pg_query.execution_time_ms) >= 10 else 0
+                    best_slower_then_10x += 1 if pg_query.execution_time_ms and \
+                                                 (yb_best.execution_time_ms / pg_query.execution_time_ms) >= 10 else 0
 
-                total += 1
+                    total += 1
+        except Exception as e:
+            self.logger.exception(e)
 
         self.json = {
             "best_picked": '{:.2f}'.format(float(yb_bests) * 100 / total),
             "qe_default": '{:.2f}'.format(qe_default_geo ** (1 / total)),
-            "qe_best": '{:.2f}'.format(qe_bests_geo ** (1 / total)) ,
+            "qe_best": '{:.2f}'.format(qe_bests_geo ** (1 / total)),
             "qo_default_vs_best": '{:.2f}'.format(qo_yb_bests_geo ** (1 / total)),
-    
+
             "total": total,
             "timeout": timed_out,
             "more_10x_default_vs_default": slower_then_10x,
